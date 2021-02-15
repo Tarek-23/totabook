89fd90b56772869c79796a2e950d63348872d8a2
tarek-23 <tarek.23@aucegypt.edu>
2021-02-15 18:12:09 +0200

    Feature: implemented and styled header

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import "./App.css";
 
+import Header from "./Header.js";
+
 function App() {
-  return <div className="app"></div>;
+  return (
+    <div className="app">
+      <Header />
+
+      <div className="app__body">
+        {/* Sidebar */}
+        {/* Feed */}
+        {/* Widgets */}
+      </div>
+    </div>
+  );
 }
 
 export default App;
